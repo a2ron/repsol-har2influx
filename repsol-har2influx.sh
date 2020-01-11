7b2a581790a21aa71bf2b79150a5aac7a174c67f
a2ron <aarr90@gmail.com>
2020-01-11 20:36:06 +0100

    fix/repsol-har2influx.sh : ./har/*.har to a new influxdb container

--- repsol-har2influx.sh
+++ repsol-har2influx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker rm -f repsol-har2influx
-node repsol.js > ./tmp/repsol.influx
+node script/repsol.js > ./tmp/repsol.influx
 docker run -d --name repsol-har2influx -p 8083:8083 -p 8086:8086 -v $PWD/tmp/repsol.influx:/data/repsol.influx influxdb 
 sleep 3
 docker exec -it repsol-har2influx influx -import -path=/data/repsol.influx -precision=ns
